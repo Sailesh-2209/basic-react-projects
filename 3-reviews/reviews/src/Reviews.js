723933fd43972f277947d667f86cf69880ebf35c
Sailesh-2209 <saileshr2209@gmail.com>
2021-01-08 23:19:47 +0530

    finished reviews

--- 3-reviews/reviews/src/Reviews.js
+++ 3-reviews/reviews/src/Reviews.js
@@ -2,8 +2,40 @@ import React, { useState } from 'react';
 import data from './data';
 
 function Reviews() {
-    const [ index, setIndex ] = useState(0);
-    const { id, name, job, image, text } = data[index];
+    const [ curIndex, setIndex ] = useState(0);
+    const { id, name, job, image, text } = data[curIndex];
+
+    const limitIndex = (index) => {
+        if (index > data.length - 1) return 0;
+        else if (index < 0) return data.length - 1;
+        else return index;
+    };
+
+    const setNext = () => {
+        setIndex((curIndex) => {
+            let newIndex = curIndex + 1;
+            return limitIndex(newIndex);
+        });
+    };
+
+    const setPrevious = () => {
+        setIndex((curIndex) => {
+            let newIndex = curIndex - 1;
+            return limitIndex(newIndex);
+        });
+    };
+
+    const setRandom = () => {
+        setIndex((curIndex) => {
+            let newIndex = Math.floor(Math.random() * data.length);
+            if (newIndex == curIndex)  {
+                return limitIndex(newIndex + 1);
+            }
+            else {
+                return limitIndex(newIndex);
+            }
+        });
+    };
 
     return(
         <>
@@ -18,10 +50,10 @@ function Reviews() {
                 </div>
                 <div className="buttons">
                     <span className="left-right-btn">
-                        <button className="left-btn">l</button>
-                        <button className="right-btn">r</button>
+                        <button onClick={setNext} className="left-btn">l</button>
+                        <button onClick={setPrevious} className="right-btn">r</button>
                     </span>
-                    <button className="random">Surprise Me</button>
+                    <button onClick={setRandom} className="random">Surprise Me</button>
                 </div>
             </div>
         </>
