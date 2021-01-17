b56baad1d71c7a781a7e0f37bdf328646e2e1ff1
Sailesh-2209 <saileshr2209@gmail.com>
2021-01-17 14:07:51 +0530

    completed tabs project

--- 6-tabs/tabs/src/App.js
+++ 6-tabs/tabs/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import { AiOutlineDoubleRight } from 'react-icons/ai';
 import './App.css';
 
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [ data, setData ] = useState([]);
+  const [ isLoading, setLoading ] = useState(true);
+  const [ value, setValue ] = useState(0);
+  const [ jobs, setJobs ] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const items = await response.json();
+    setData(items);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return(
+      <Loading />
+    );
+  }
+
+  const { id, order, title, dates, duties, company } = data[value];
+  console.log(company);
+  return(
+    <>
+      <div className="job-btns">
+        {
+          data.map((item, index) => {
+            return <button onClick={() => setValue(index)} key={index} >{item.company}</button>;
+          })
+        }
+      </div>
+      <div className="info">
+        <h1>{title}</h1>
+        <h3>{company}</h3>
+        <p>{dates}</p>
+        {
+          duties.map((duty, index) => {
+            return(
+              <div className="duty" key={index} ><AiOutlineDoubleRight /> {duty}</div>
+            );
+          })
+        }
+      </div>
+    </>
   );
 }
 
